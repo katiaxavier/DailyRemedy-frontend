25477d87bca316c807fef8f3aef2036931f723ba
katia <ktia_kurenai@hotmail.com>
2020-04-12 22:26:44 -0400

    Logon

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api';
 import './styles.css'
 
 import remedyImg from '../../assets/remedy.png';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('userId', id);
+      localStorage.setItem('userName', response.data.name);
+
+      history.push('/profile');
+
+    } catch (error) {
+      alert(`${error.error}`)
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={remedyImg} alt="remedy" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)} />
           <button className="button" type="submit">Entrar</button>
 
           <Link className="back-link" to="/register">
