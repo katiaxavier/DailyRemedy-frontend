e7192a30830e3b762325d2fc8a5168e67e601449
kahkurenai <sys1@grupopmz.com.br>
2020-04-15 19:11:53 -0400

    Tabela de remedios por turno

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+/* eslint-disable no-unused-expressions */
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function Profile() {
+  const turnos = ["Madrugada", "Manhã", "Tarde", "Noite"];
+  const [remedys, setRemedys] = useState([]);
+
+  const userId = localStorage.getItem('userId');
+  const userName = localStorage.getItem('userName');
+
+  useEffect(() => {
+    api.get('profiles', {
+      headers: {
+        Authorization: userId,
+      }
+    }).then(response => {
+      setRemedys(response.data);
+    })
+  }, [userId]);
+
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Daily Remedy" />
-        <span>Bem vindo(a), Kátia Xavier</span>
+        <span>Bem vindo(a), {userName}</span>
 
         <Link className="button" to="/remedys/new">
           Cadastrar novo medicamento
@@ -21,122 +41,44 @@ export default function Profile() {
         </button>
       </header>
 
-      <div>
+      {turnos.map(turno => {
+        return (
+          <table >
+            <tr>
+              <th className="th" colSpan="5">
+                <p>{turno}</p>
+              </th>
+            </tr>
 
-        <table>
-          <tr>
-          <th className="th" colspan="5">
-              <p>MANHÃ</p>
-            </th>
-          </tr>
+            <tr className="back-tr">
+              <th>Remédio</th>
+              <th>Observação</th>
+              <th>Qtd</th>
+              <th>Horas</th>
+              <th></th>
+            </tr>
 
-          <tr className="back-tr">
-            <th>Remédio</th>
-            <th>Observação</th>
-            <th>Qtd</th>
-            <th>Horas</th>
-            <th></th>
-          </tr>
-
-          <tr>
-            <th>DIAMICRON 60mg</th>
-            <th>Jejum</th>
-            <th>2</th>
-            <th>07:00</th>
-
-            <th>
-              <button type="button">
-                <FiTrash2 size={20} color="#295872" />
-              </button>
-            </th>
-
-          </tr>
-
-          <tr>
-            <th>DIAMICRON 60mg</th>
-            <th>Jejum</th>
-            <th>2</th>
-            <th>07:00</th>
-
-            <th>
-              <button type="button">
-                <FiTrash2 size={20} color="#295872" />
-              </button>
-            </th>
-
-          </tr>
-
-          <tr>
-            <th>DIAMICRON 60mg</th>
-            <th>Jejum</th>
-            <th>2</th>
-            <th>07:00</th>
-
-
-            <th>
-              <button type="button">
-                <FiTrash2 size={20} color="#295872" />
-              </button>
-            </th>
-
-          </tr>
-
-        </table>
-      </div>
-
-      <table>
-
-        <tr>
-          <th className="th" colspan="5">
-            <p>TARDE</p>
-          </th>
-        </tr>
-
-        <tr className="back-tr">
-          <th>Remedio</th>
-          <th>Observação</th>
-          <th>Qtd</th>
-          <th>Horas</th>
-          <th></th>
-        </tr>
-
-        <tr>
-          <th>DIAMICRON 60mg</th>
-          <th>Jejum</th>
-          <th>2</th>
-          <th>07:00</th>
-          <th>
-            <button type="button">
-              <FiTrash2 size={20} color="#295872" />
-            </button>
-          </th>
-        </tr>
-
-        <tr>
-          <th>DIAMICRON 60mg</th>
-          <th>Jejum</th>
-          <th>2</th>
-          <th>07:00</th>
-          <th>
-            <button type="button">
-              <FiTrash2 size={20} color="#295872" />
-            </button>
-          </th>
-        </tr>
-
-        <tr>
-          <th>DIAMICRON 60mg</th>
-          <th>Jejum</th>
-          <th>2</th>
-          <th>07:00</th>
-          <th>
-            <button type="button">
-              <FiTrash2 size={20} color="#295872" />
-            </button>
-          </th>
-        </tr>
-
-      </table>
+            {remedys.map(remedy => {
+              if (remedy.shift === turno) {
+                return (
+                  <tr key={remedy.id}>
+                    <th>{remedy.name}</th>
+                    <th>{remedy.description}</th>
+                    <th>{remedy.shift}</th>
+                    <th>{remedy.hour}</th>
+                    <th>
+                      <button type="button">
+                        <FiTrash2 size={20} color="#295872" />
+                      </button>
+                    </th>
+                  </tr>
+                )
+              }
+            })}
+          </table>
+        )
+      })
+      }
     </div>
 
   );
